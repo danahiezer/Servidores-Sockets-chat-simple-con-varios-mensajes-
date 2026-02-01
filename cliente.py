8dc9c01975c857bfa365e39f6b199d109f93053a
danahiezer <dansantacruz5@gmail.com>
2026-02-01 09:39:55 -0300

    bucle en para enviar varios mensajes

--- cliente.py
+++ cliente.py
@@ -7,14 +7,19 @@ cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
 puerto = 5000
 cliente.connect((host, puerto))
+while True:
+    # Enviar mensaje
+    mensaje = input("tu: ")
+    cliente.send(mensaje.encode('utf-8'))
 
-# Enviar mensaje
-mensaje = "Hola servidor!"
-cliente.send(mensaje.encode('utf-8'))
+    if mensaje.lower() == "adios":
+        # Recibir respuesta
+        respuesta = cliente.recv(1024).decode('utf-8')
+        print(f"Servidor responde: {respuesta}")
+        break
 
-# Recibir respuesta
-respuesta = cliente.recv(1024).decode('utf-8')
-print(f"Servidor responde: {respuesta}")
+    respuesta = cliente.recv(1024).decode("utf-8")
+    print(f"servidor: {respuesta}")
 
 # Cerrar
 cliente.close()
