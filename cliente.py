6bb7fbb40ee1343567c576c327d87a55217d0cb9
danahiezer <dansantacruz5@gmail.com>
2026-02-01 12:03:49 -0300

    depuracion e implementacion de comentarios

--- cliente.py
+++ cliente.py
@@ -5,7 +5,7 @@ cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Conectar al servidor
 cliente.connect(("localhost", 5000))
-
+# recibe la bienvenida del servidor
 bienvenida = cliente.recv(1024).decode("utf-8")
 print(f"servidor: {bienvenida} ")
 
@@ -13,10 +13,10 @@ while True:
     # Enviar mensaje
     mensaje = input("tu: ")
     cliente.send(mensaje.encode('utf-8'))
-
+    # recibe respuesta del servidor
     respuesta = cliente.recv(1024).decode("utf-8")
     print(f"servidor: {respuesta}")
-
+    # si el usuario pone adios. se desconecta del servidor
     if mensaje.lower() == "adios":
         break
 
--- servidor.py
+++ servidor.py
@@ -11,22 +11,23 @@ def manejaClientes(conexion,direccion):
         try:
             # Recibir datos
             mensaje = conexion.recv(1024).decode('utf-8')
-            
+
+            # si no hay mensaje notifica que cliente se desconecto
             if not mensaje:
                 print(f"el cliente {direccion} se desconecto")
                 break
             print(f"[{direccion}]: {mensaje}")
-
+            # si el cliente dice adios se desconecta y recibe mensaje despedida
             if mensaje.lower() == "adios":
                 conexion.send("hasta luego".encode("utf-8"))
                 break
-
+            # devuelve las respuestas del servidor en mayusculas
             respuesta = mensaje.upper()
             conexion.send(respuesta.encode("utf-8"))
 
         except:
             print(f"error con cliente {direccion}")
-        
+            break
     conexion.close()
     print(f"conexion cerrada con [{direccion}]")
 
@@ -40,17 +41,20 @@ puerto = 5000
 # Enlazar el socket a la dirección
 servidor.bind((host, puerto))
 
-# Escuchar conexiones (máximo 1 en espera)
-servidor.listen(4)
+# Escuchar conexiones (máximo 3 en espera)
+servidor.listen(3)
 print(f"Servidor milti-cliente escuchando en {host}:{puerto}")
 try:
     while True:
         # Aceptar una conexión
         conexion, direccion = servidor.accept()
         print(f"Conexión establecida con {direccion}")
-
+        # crea hilo para cada cliente y asi poder atender a todos a la vez
         hilo = threading.Thread(target=manejaClientes, args=(conexion,direccion))
         hilo.start()
+
+        print(f"Clientes activos: {threading.active_count() - 1}")
+
 except KeyboardInterrupt:
     print(f"servidor detenido por el usuario")
     servidor.close()
